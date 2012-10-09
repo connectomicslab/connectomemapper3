bc217a162cf4e6ca3261def9c588b77f1bd646a1
Adrien Birbaumer <abirbaumer@gmail.com>
2012-10-09 10:01:46 +0200

    Implemented segmentation workflow.

--- cmp/stages/segmentation/segmentation.py
+++ cmp/stages/segmentation/segmentation.py
@@ -1,38 +1,52 @@
 
+# General imports
 try: 
-	from traits.api import *
+    from traits.api import *
 except ImportError: 
-	from enthought.traits.api import *
+    from enthought.traits.api import *
 try: 
-	from traitsui.api import *
+    from traitsui.api import *
 except ImportError: 
-	from enthought.traits.ui.api import *
+    from enthought.traits.ui.api import *
+	
+# Nipype imports
+import nipype.pipeline.engine as pe
+import nipype.interfaces.utility as util
+import nipype.interfaces.freesurfer as fs
+
+# Own imports
 from cmp.stages.common import CMP_Stage
 
 class Segmentation_Config(HasTraits):
-	pass
-	#process_type = Enum('Diffusion',['Diffusion'])
+    freesurfer_subjects_dir = Str
+    traits_view = View()
 	
 class Segmentation(CMP_Stage):
-	# General and UI members
-	name = 'Segmentation'
-	display_color = 'lightyellow'
-	position_x = 45
-	position_y = 320
-	config = Segmentation_Config()
+    # General and UI members
+    name = 'Segmentation'
+    display_color = 'lightyellow'
+    position_x = 45
+    position_y = 320
+    config = Segmentation_Config()
 	
-		
-	#def create_workflow(self):
-		#flow = pe.Workflow(name="CMP_Parcellation")
-		
-		#input_node = pe.Node(interface=util.IdentityInterface(fields=["aparc+aseg"]),name="inputnode")
+    	
+    def create_workflow(self):
+        flow = pe.Workflow(name="Segmentation_stage")
+        
+        inputnode = pe.Node(interface=util.IdentityInterface(fields=["T1"]),name="inputnode")
+        
+        # Converting to .mgz format
+        fs_mriconvert = pe.Node(interface=fs.MRIConvert(out_type="mgz",out_file="T1.mgz"),name="fs_mriconvert")
+        
+        # ReconAll => named outputnode as we don't want to select a specific output....
+        fs_reconall = pe.Node(interface=fs.ReconAll(),name="outputnode")
+        fs_reconall.inputs.subjects_dir = self.config.freesurfer_subjects_dir
+        
+        flow.connect([
+                    (inputnode,fs_mriconvert,[('T1','in_file')]),
+                    (fs_mriconvert,fs_reconall,[('out_file','T1_files')]),
+                    ])
+        
+        return flow
 
-		#if self.config.parcellation_scheme == 'NativeFreesurfer':
-		#	...
-			
-		#output_node = pe.Node(interface=util.IdentityInterface(fields=["WM_mask_1mm","GM_masks_1mm"]),name="outputnode")
-		
-		#flow.connect([(inputnode,...[(...,...),...]),...])
-		
-		#return flow
 	
