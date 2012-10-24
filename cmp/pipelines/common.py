95e72ba63bdc41bf960a621e13657ccd4c04774d
Adrien Birbaumer <abirbaumer@gmail.com>
2012-10-24 16:59:40 +0200

    Minor modifications

--- cmp/pipelines/common.py
+++ cmp/pipelines/common.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2009-2012, Ecole Polytechnique Federale de Lausanne (EPFL) and
+# Hospital Center and University of Lausanne (UNIL-CHUV), Switzerland
+# All rights reserved.
+#
+#  This software is distributed under the open-source license Modified BSD.
+
+""" Common functions for CMP pipelines
+""" 
 
 import os
 import shutil
