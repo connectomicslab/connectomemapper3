b36adc705ed3e566e56391f01e99a6a626bfe367
Adrien Birbaumer <adrien.birbaumer@gmail.com>
2012-12-19 15:43:48 +0100

    v2.0.1. Added precess status window.

--- cmp/pipelines/common.py
+++ cmp/pipelines/common.py
@@ -9,7 +9,11 @@
 
 import os
 import shutil
+import threading
+import time
 from nipype.utils.filemanip import copyfile
+import nipype.pipeline.engine as pe
+import nipype.interfaces.utility as util
 import nipype.interfaces.diffusion_toolkit as dtk
 import nipype.interfaces.fsl as fsl
 import nipype.interfaces.freesurfer as fs
@@ -17,6 +21,122 @@ from nipype.caching import Memory
 from nipype.interfaces.base import BaseInterface, \
     BaseInterfaceInputSpec, File, TraitedSpec, isdefined
 from nipype.utils.filemanip import split_filename
+
+from traits.api import *
+from traitsui.api import *
+
+class ProgressWindow(HasTraits):
+    main_status = Str("Processing launched...")
+    stages_status = List([''])
+    
+    traits_view = View(Group(
+                            Group(
+                                Item('main_status',show_label=False,style='readonly'),
+                                label = 'Main status', show_border=True),
+                            Group(
+                                Item('stages_status',show_label=False,editor=ListStrEditor(),enabled_when='2<1'),
+                                label = 'Stages status', show_border=True),
+                            ),
+                       width=300,
+                       height=400,
+                       buttons=['OK','Cancel'],
+                       title='Processing status',
+                       kind='livemodal')
+    
+class ProgressThread(threading.Thread):
+    stages = {}
+    stage_names = []
+    pw = Instance(ProgressWindow)
+    
+    def run(self):
+        c=1
+        while(c < len(self.stage_names)):
+            print "******* checking progress *****"
+            
+            c = 1
+            statuses = []
+            for stage in self.stage_names:
+                if self.stages[stage].has_run():
+                    statuses.append(stage+" stage finished!")
+                    c = c+1
+                else:
+                    statuses.append(stage+" stage running...")
+            self.pw.stages_status = statuses
+            time.sleep(5)
+        self.pw.main_status = "Processing finished!"
+                    
+class ProcessThread(threading.Thread):
+    pipeline = Instance(Any)
+    
+    def run(self):
+       self.pipeline.process()
+                      
+
+class Pipeline(HasTraits):
+    # informations common to project_info
+    base_directory = Directory
+    last_date_processed = Str
+    last_stage_processed = Str
+    
+    # num core settings
+    number_of_cores = Enum(1,[1,2,3,4])
+    
+    traits_view = View(HGroup(
+                        Include('pipeline_group'),
+                        Group(
+                            Group(
+                                Item('base_directory',enabled_when='1>2',show_label=False,width=300),
+                                label='Base directory',
+                            ),
+                            Group(
+                                Item('pipeline_name',style='readonly'),
+                                Item('last_date_processed',style='readonly'),
+                                Item('last_stage_processed',style='readonly'),
+                                label='Last processing'
+                            ),
+                            Group(
+                                'number_of_cores',
+                                label='Processing configuration'
+                            ),
+                            
+                        ),
+                        
+                        ),kind = 'livemodal')
+    
+    def __init__(self, project_info):
+        self.base_directory = project_info.base_directory
+        self.last_date_processed = project_info.last_date_processed
+        for stage in self.stages.values():
+            stage.stage_dir = os.path.join(self.base_directory,"NIPYPE",self.pipeline_name,stage.name)
+        
+    def create_stage_flow(self, stage_name):
+        stage = self.stages[stage_name]
+        flow = pe.Workflow(name=stage.name)
+        inputnode = pe.Node(interface=util.IdentityInterface(fields=stage.inputs),name="inputnode")
+        outputnode = pe.Node(interface=util.IdentityInterface(fields=stage.outputs),name="outputnode")
+        flow.add_nodes([inputnode,outputnode])
+        stage.create_workflow(flow,inputnode,outputnode)
+        return flow
+        
+    def fill_stages_outputs(self):
+        for stage in self.stages.values():
+            if stage.enabled:
+                stage.define_inspect_outputs()
+                
+    def launch_progress_window(self):
+        pw = ProgressWindow()
+        pt = ProgressThread()
+        pt.pw = pw
+        pt.stages = self.stages
+        pt.stage_names = self.ordered_stage_list
+        pt.start()
+        pw.configure_traits()
+        
+    def launch_process(self):
+        pt = ProcessThread()
+        pt.pipeline = self
+        pt.start()
+               
 
 def convert_rawdata(base_directory, input_dir, out_prefix):
     file_list = os.listdir(input_dir)
