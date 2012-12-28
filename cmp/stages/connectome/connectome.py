dc2d6388e1903bfa95c408a77bf59e222d99b570
Adrien Birbaumer <adrien.birbaumer@gmail.com>
2012-12-28 14:34:51 +0100

    Fixed CFF conversion

--- cmp/stages/connectome/connectome.py
+++ cmp/stages/connectome/connectome.py
@@ -18,7 +18,6 @@ import gzip
 # Nipype imports
 import nipype.interfaces.utility as util
 import nipype.pipeline.engine as pe
-import nipype.interfaces.fsl as fsl
 import nipype.interfaces.cmtk as cmtk
 from nipype.interfaces.base import BaseInterface, BaseInterfaceInputSpec,\
     traits, File, TraitedSpec, InputMultiPath, OutputMultiPath, isdefined
@@ -30,28 +29,13 @@ from cmp.stages.common import Stage
 
 class ConnectomeConfig(HasTraits):
     compute_curvature = Bool(True)
-    cff_enabled_trait = Bool(False)
-    cff_creator = Str
-    cff_email = Str
-    cff_publisher = Str
-    cff_license = Str
-    
     output_types = List(['gPickle'], editor=CheckListEditor(values=['gPickle','mat','cff','graphml'],cols=4))
 
     traits_view = View(Item('output_types',style='custom'),
                         Group('compute_curvature',label='Connectivity matrix', show_border=True),
-                        Group('cff_creator','cff_email','cff_publisher','cff_license',
-                        label='CFF creation metadata', show_border=True, visible_when='cff_enabled_trait==True'
-                        ),
-                        kind='live',
                         )
-                        
-    def _output_type_changed(self, new):
-        if 'cff' in new:
-            self.cff_enabled_trait = True
-        else:
-            self.cff_enabled_trait = False
-                        
+
+
 class CMTK_cmatInputSpec(BaseInterfaceInputSpec):
     track_file = File(desc='Tractography result', exists=True, mandatory=True)
     roi_volumes = InputMultiPath(File(exists=True), desc='ROI volumes registered to diffusion space')
@@ -89,7 +73,7 @@ class CMTK_cmat(BaseInterface):
             cvt.inputs.title = 'Connectome mapper'
             cvt.inputs.nifti_volumes = self.inputs.roi_volumes
             cvt.inputs.tract_files = ['streamline_final.trk']
-            cvt.inputs.gpickled_networks = ['connectome_%s.gpickle' % parkey]
+            cvt.inputs.gpickled_networks = glob.glob(os.path.abspath("connectome_*.gpickle"))
             cvt.run()
              
         return runtime
@@ -105,13 +89,12 @@ class CMTK_cmat(BaseInterface):
         outputs['connectivity_matrices'] = glob.glob(os.path.abspath('connectome*'))
         
         return outputs
-    
 
 class ConnectomeStage(Stage):
     name = 'connectome_stage'
     config = ConnectomeConfig()
-    inputs = ["diffusion_b0_resampled","roi_volumes","track_file",
-              "parcellation_scheme","gFA","skewness","kurtosis","P0","T1-TO-B0_mat"]
+    inputs = ["roi_volumes_registered","track_file",
+              "parcellation_scheme","gFA","skewness","kurtosis","P0"]
     outputs = ["endpoints_file","endpoints_mm_file","final_fiberslength_files",
                "filtered_fiberslabel_files","final_fiberlabels_files",
                "streamline_final_file","connectivity_matrices"]
@@ -124,27 +107,27 @@ class ConnectomeStage(Stage):
 
         # Additional maps
         map_merge = pe.Node(interface=util.Merge(4),name="merge_additional_maps")
-        
-        # Register ROI Volumes to B0 space
-        fsl_applyxfm = pe.MapNode(interface=fsl.ApplyXfm(apply_xfm=True, interp="nearestneighbour"),name="apply_registration",iterfield=["in_file"])
-        
+
         flow.connect([
                      (inputnode,map_merge, [('gFA','in1'),('skewness','in2'),('kurtosis','in3'),('P0','in4')]),
-                     (inputnode,fsl_applyxfm, [('roi_volumes','in_file'),('diffusion_b0_resampled','reference'),('T1-TO-B0_mat','in_matrix_file')]),
-                     (inputnode,cmtk_cmat, [('track_file','track_file'),('parcellation_scheme','parcellation_scheme')]),
-                     (fsl_applyxfm,cmtk_cmat, [('out_file','roi_volumes')]),
+                     (inputnode,cmtk_cmat, [('track_file','track_file'),('parcellation_scheme','parcellation_scheme'),('roi_volumes_registered','roi_volumes')]),
                      (map_merge,cmtk_cmat, [('out','additional_maps')]),
                      (cmtk_cmat,outputnode, [('endpoints_file','endpoints_file'),('endpoints_mm_file','endpoints_mm_file'),
                              ('final_fiberslength_files','final_fiberslength_files'),('filtered_fiberslabel_files','filtered_fiberslabel_files'),
                              ('final_fiberlabels_files','final_fiberlabels_files'),('streamline_final_file','streamline_final_file'),
                              ('connectivity_matrices','connectivity_matrices')])
                      ])
-
+    
     def define_inspect_outputs(self):
         con_results_path = os.path.join(self.stage_dir,"compute_matrice","result_compute_matrice.pklz")
         if(os.path.exists(con_results_path)):
             con_results = pickle.load(gzip.open(con_results_path))
             self.inspect_outputs_dict['streamline_final'] = ['trackvis',con_results.outputs.streamline_final_file]
+            
+            for mat in con_results.outputs.connectivity_matrices:
+                if 'gpickle' in mat:
+                    self.inspect_outputs_dict[os.path.basename(mat)] = ["showmatrix_gpickle",mat, "number_of_fibers", "True"]
+                
             self.inspect_outputs = self.inspect_outputs_dict.keys()
 
     def has_run(self):
