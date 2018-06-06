9e5c3b125b7cb69d483bcec010346cfd25341886
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2018-06-06 12:38:51 +0200

    FIX: ACT tractrography and seeding from GM/WM interface

--- cmp/stages/diffusion/tracking.py
+++ cmp/stages/diffusion/tracking.py
@@ -955,7 +955,7 @@ def create_mrtrix_tracking_flow(config):
 
     elif config.tracking_mode == 'Probabilistic':
         mrtrix_seeds = pe.Node(interface=make_mrtrix_seeds(),name="mrtrix_seeds")
-        mrtrix_tracking = pe.MapNode(interface=StreamlineTrack(),name="mrtrix_probabilistic_tracking",iterfield=['seed_file'])
+        mrtrix_tracking = pe.MapNode(interface=StreamlineTrack(),name="mrtrix_probabilistic_tracking")
         mrtrix_tracking.inputs.desired_number_of_tracks = config.desired_number_of_tracks
         #mrtrix_tracking.inputs.maximum_number_of_seeds = config.max_number_of_seeds
         mrtrix_tracking.inputs.maximum_tract_length = config.max_length
