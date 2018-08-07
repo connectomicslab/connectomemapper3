5d9fa74e4bb73e5631d9d607c434ac6d6028495b
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2018-08-07 16:58:49 +0200

    ENH: implementation of run.py for BIDS apps in progress
    Former-commit-id: 00853ad149b81adfeb7a3a10af720c8357cde339

--- cmp/run.py
+++ cmp/run.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python2
+# -*-coding:Latin-1 -*
+
 # Copyright (C) 2009-2017, Ecole Polytechnique Federale de Lausanne (EPFL) and
 # Hospital Center and University of Lausanne (UNIL-CHUV), Switzerland
 # All rights reserved.
@@ -13,17 +15,18 @@ import numpy
 from glob import glob
 
 #import cmp.gui
-import cmp.project
-from cmp.info import __version__
+import gui
+import project
+from info import __version__
 
 # __version__ = open(os.path.join(os.path.dirname(os.path.realpath(__file__)),
 #                                 'version')).read()
 
 def info():
     print "\nConnectome Mapper (CMP) " + __version__
-    print """Copyright (C) 2009-2018, Ecole Polytechnique Fédérale de Lausanne (EPFL) and
+    print unicode("""Copyright (C) 2009-2018, Ecole Polytechnique Fédérale de Lausanne (EPFL) and
              Hospital Center and University of Lausanne (UNIL-CHUV), Switzerland
-             All rights reserved.\n"""
+             All rights reserved.\n""")
 
 # Checks the needed dependencies. We call directly the functions instead
 # of just checking existence in $PATH in order to handl missing libraries.
@@ -45,25 +48,53 @@ def dep_check():
                 FREESURFER_HOME variable is exported and the SetUpFreeSurfer.sh setup
                 script is sourced."""
 
-    # Check for MRtrix
-    # if subprocess.call("mrconvert",stdout=nul,stderr=nul,shell=True) != 255:
-    #     error = """MRtrix3 not installed or not working correctly. Check that PATH variable is updated with MRtrix3 binary (bin) directory."""
-
-    # Check for DTK
-    #     if subprocess.call("dti_recon",stdout=nul,stderr=nul,shell=True) != 0 or "DSI_PATH" not in os.environ:
-    #         error = """Diffusion Toolkit not installed or not working correctly. Check that
-    # the DSI_PATH variable is exported and that the dtk binaries (e.g. dti_recon) are in
-    # your path."""
-
-    # Check for DTB
-    #     if subprocess.call("DTB_dtk2dir",stdout=nul,stderr=nul,shell=True) != 1:
-    #         error = """DTB binaries not installed or not working correctly. Check that the
-    # DTB binaries (e.g. DTB_dtk2dir) are in your path and don't give any error."""
-
     if error != "":
         print error
         sys.exit(2)
 
+def create_cmp_command(project):
+
+    if len(project.subject_sessions)>0:
+        cmd = "connectomemapper3 %s %s %s %s %s %s %s %s %s"%(project.base_directory,project.subject,project.subject_session,project.anat_config_file,True,project.dmri_config_file,True,project.fmri_config_file,True)
+    else:
+        cmd = "connectomemapper3 %s %s %s %s %s %s %s %s"%(project.base_directory,project.subject,project.anat_config_file,True,project.dmri_config_file,True,project.fmri_config_file,True)
+
+    return cmd
+
+def readLineByLine(filename):
+    with open(filename, 'r') as f: #Use with statement to correctly close the file when you read all the lines.
+        for line in f:    # Use implicit iterator over filehandler to minimize memory used
+            yield line.strip('\n') #Use generator, to minimize memory used, removing trailing carriage return as it is not part of the command.
+
+def create_subject_configuration_from_ref(project, ref_conf_file, pipeline_type):
+
+    subject_derivatives_dir = os.path.join(project.base_directory,"derivatives")
+
+    if len(project.subject_sessions) > 0: #Session structure
+        subject_conf_file = os.path.join(subject_derivatives_dir,"{}_{}_{}_config.ini".format(project.subject,project.subject_session,pipeline_type))
+    else:
+        subject_conf_file = os.path.join(subject_derivatives_dir,"{}_{}_config.ini".format(project.subject,pipeline_type))
+
+    if os.path.isfile(subject_conf_file):
+        print "WARNING: rewriting config file {}".format(subject_conf_file)
+        os.remove(subject_conf_file)
+
+    #Copy and edit appropriate fields/lines
+    f = open(subject_conf_file,'w')
+    for line in readLineByLine(ref_conf_file):
+        if "subject = " in line:
+            f.write("subject = {}".format(project.subject) + os.linesep)
+        elif "subjects = " in line:
+            f.write("subjects = {}".format(project.subjects) + os.linesep)
+        elif "subject_sessions = " in line:
+            f.write("subject_sessions = {}".format(project.subject_sessions) + os.linesep)
+        elif "subject_session = " in line:
+            f.write("subject_session = {}".format(project.subject_session) + os.linesep)
+        else:
+            f.write(line)
+    f.close()
+
+    return subject_conf_file
 
 def run(command, env={}):
     merged_env = os.environ
@@ -131,19 +162,63 @@ else:
     subject_dirs = glob(os.path.join(args.bids_dir, "sub-*"))
     subjects_to_analyze = [subject_dir.split("-")[-1] for subject_dir in subject_dirs]
 
+project = cmp.gui.CMP_Project_Info()
+project.base_directory = args.bids_dir
+project.subjects = ['sub-{}'.format(label) for label in subjects_to_analyze]
+
+#Derivatives directory creation if it does not exist
+cmp_derivatives_dir = os.path.join(args.bids_dir, "derivatives" , "cmp")
+if not os.path.isdir(cmp_derivatives_dir):
+    os.mkdir(cmp_derivatives_dir)
+
 # running participant level
 if args.analysis_level == "participant":
 
     # find all T1s and skullstrip them
     for subject_label in subjects_to_analyze:
 
+        project.subject = 'sub-{}'.format(subject_label)
 
-        for T1_file in glob(os.path.join(args.bids_dir, "sub-%s"%subject_label,
-                                         "anat", "*_T1w.nii*")) + glob(os.path.join(args.bids_dir,"sub-%s"%subject_label,"ses-*","anat", "*_T1w.nii*")):
-            out_file = os.path.split(T1_file)[-1].replace("_T1w.", "_brain.")
-            # cmd = "bet %s %s"%(T1_file, os.path.join(args.output_dir, out_file))
-            # print(cmd)
-            # run(cmd)
+        # Check if multiple session (sub-XX/ses-YY/anat/... structure or sub-XX/anat.. structure?)
+        subject_session_dirs = glob(os.path.join(args.bids_dir, project.subject, "ses-*"))
+        project.subject_sessions = ['ses-{}'.format(subject_session_dir.split("-")[-1]) for subject_session_dir in subject_session_dirs]
+
+        if len(project.subject_sessions) > 0: #Session structure
+
+            for session in project.subject_sessions:
+                project.subject_session = session
+
+                #Derivatives folder creation (Folder is first deleted if it already exists)
+                session_derivatives_dir = os.path.join(cmp_derivatives_dir, project.subject, project.subject_session)
+                if not os.path.isdir(session_derivatives_dir):
+                    os.makedirs(session_derivatives_dir)
+
+                project.anat_config_file = create_subject_configuration_from_ref(project,args.anat_pipeline_config)
+                project.dmri_config_file = create_subject_configuration_from_ref(project,args.dwi_pipeline_config)
+                project.fmri_config_file = create_subject_configuration_from_ref(project,args.func_pipeline_config)
+
+                cmd = create_cmp_command(project=project)
+                run(command=cmd)
+
+
+        else: #No session structure
+            project.subject_sessions = ['']
+            project.subject_session = ''
+
+            #Derivatives folder creation (Folder is first deleted if it already exists)
+            subject_derivatives_dir = os.path.join(cmp_derivatives_dir, project.subject)
+            if not os.path.isdir(subject_derivatives_dir):
+                os.makedirs(subject_derivatives_dir)
+
+            project.anat_config_file = create_subject_configuration_from_ref(project,args.anat_pipeline_config,'anatomical')
+            project.dmri_config_file = create_subject_configuration_from_ref(project,args.dwi_pipeline_config,'diffusion')
+            project.fmri_config_file = create_subject_configuration_from_ref(project,args.func_pipeline_config,'fMRI')
+
+            cmd = create_cmp_command(project=project)
+
+            print cmd
+            #run(command=cmd)
+
 
 # running group level; ultimately it will compute average connectivity matrices
 # elif args.analysis_level == "group":
