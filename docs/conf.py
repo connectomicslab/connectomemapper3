b024ed744134280207adb116743f79f2cce45346
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2018-11-26 13:46:25 +0100

    Updated docs
    Former-commit-id: cf22098cc0899882b2f0fd953ba95747f2eba002

--- docs/conf.py
+++ docs/conf.py
@@ -143,7 +143,7 @@ html_last_updated_fmt = '%b %d, %Y'
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {'**': ['sidebar_version.html', 'localtoc.html','relations.html', 'sourcelink.html', 'searchbox.html']}
+html_sidebars = {'**': ['sidebar_version.html', 'localtoc.html','relations.html', 'sourcelink.html', 'searchbox.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
