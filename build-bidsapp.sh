b9a210a92ba8bad87051cf743c7b3b109f74c8e4
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2019-02-15 10:09:02 +0100

    Refactoring of setup and dockerfile after merge
    Former-commit-id: 3ac9cf8428e8c7fedd785acbcc9ecaebb0d8ac94

--- build-bidsapp.sh
+++ build-bidsapp.sh
@@ -2,10 +2,10 @@
 CMP_BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 echo $CMP_BUILD_DATE
 
-VERSION=$(python ../connectomemapper3/get_version.py)
+VERSION=$(python get_version.py)
 echo $VERSION
 
-VCS_REF=$(git rev-parse --verify HEAD --git-path ../connectomemapper3 | tail -1)
+VCS_REF=$(git rev-parse --verify HEAD)
 echo $VCS_REF
 
 docker build --no-cache --rm --build-arg password=${BITBUCKET_PWD} --build-arg BUILD_DATE=$CMP_BUILD_DATE --build-arg VERSION=$VERSION --build-arg VCS_REF=$VCS_REF -t sebastientourbier/connectomemapper-bidsapp:dev .
