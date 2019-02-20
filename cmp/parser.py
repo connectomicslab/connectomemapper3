546d1fc935c6fb9a8e1684ceaa5506bdba710e1b
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2019-02-20 10:44:33 +0100

    ENH: command line argument parser in a separate file for easy integration in documentation
    Former-commit-id: a5b4bf90561ba297ced964ceac6cd975cc9edb89

---
+++ cmp/parser.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2017-2019, Brain Communication Pathways Sinergia Consortium, Switzerland
+# All rights reserved.
+#
+#  This software is distributed under the open-source license Modified BSD.
+
+""" Multi-scale Brain Parcellator Commandline Parser
+"""
+
+from info import __version__
+from info import __release_date__
+
+def get():
+    import argparse
+    p = = argparse.ArgumentParser(description='Example BIDS App entrypoint script.')
+    p.add_argument('bids_dir', help='The directory with the input dataset '
+                        'formatted according to the BIDS standard.')
+    p.add_argument('output_dir', help='The directory where the output files '
+                        'should be stored. If you are running group level analysis '
+                        'this folder should be prepopulated with the results of the'
+                        'participant level analysis.')
+    p.add_argument('analysis_level', help='Level of the analysis that will be performed. '
+                        'Multiple participant level analyses can be run independently '
+                        '(in parallel) using the same output_dir.',
+                        choices=['participant', 'group'])
+    p.add_argument('--participant_label', help='The label(s) of the participant(s) that should be analyzed. The label '
+                       'corresponds to sub-<participant_label> from the BIDS spec '
+                       '(so it does not include "sub-"). If this parameter is not '
+                       'provided all subjects should be analyzed. Multiple '
+                       'participants can be specified with a space separated list.',
+                       nargs="+")
+
+    p.add_argument('--anat_pipeline_config', help='Configuration .txt file for processing stages of the anatomical MRI processing pipeline')
+    p.add_argument('--dwi_pipeline_config', help='Configuration .txt file for processing stages of the diffusion MRI processing pipeline')
+    p.add_argument('--func_pipeline_config', help='Configuration .txt file for processing stages of the fMRI processing pipeline')
+
+    # p.add_argument('--skip_bids_validator', help='Whether or not to perform BIDS dataset validation',
+    #                    action='store_true')
+    p.add_argument('-v', '--version', action='version',
+                        version='BIDS-App example version {}'.format(__version__))
+    return p
--- run.py
+++ run.py
@@ -25,6 +25,7 @@ from glob import glob
 #Own imports
 import cmp.project as project
 from cmp.info import __version__
+from cmp import parser
 
 # __version__ = open(os.path.join(os.path.dirname(os.path.realpath(__file__)),
 #                                 'version')).read()
@@ -156,35 +157,8 @@ def run(command, env={}, log_filename={}):
     # if process.returncode != 0:
     #     raise Exception("Non zero return code: %d"%process.returncode)
 
-parser = argparse.ArgumentParser(description='Example BIDS App entrypoint script.')
-parser.add_argument('bids_dir', help='The directory with the input dataset '
-                    'formatted according to the BIDS standard.')
-parser.add_argument('output_dir', help='The directory where the output files '
-                    'should be stored. If you are running group level analysis '
-                    'this folder should be prepopulated with the results of the'
-                    'participant level analysis.')
-parser.add_argument('analysis_level', help='Level of the analysis that will be performed. '
-                    'Multiple participant level analyses can be run independently '
-                    '(in parallel) using the same output_dir.',
-                    choices=['participant', 'group'])
-parser.add_argument('--participant_label', help='The label(s) of the participant(s) that should be analyzed. The label '
-                   'corresponds to sub-<participant_label> from the BIDS spec '
-                   '(so it does not include "sub-"). If this parameter is not '
-                   'provided all subjects should be analyzed. Multiple '
-                   'participants can be specified with a space separated list.',
-                   nargs="+")
-
-parser.add_argument('--anat_pipeline_config', help='Configuration .txt file for processing stages of the anatomical MRI processing pipeline')
-parser.add_argument('--dwi_pipeline_config', help='Configuration .txt file for processing stages of the diffusion MRI processing pipeline')
-parser.add_argument('--func_pipeline_config', help='Configuration .txt file for processing stages of the fMRI processing pipeline')
-
-# parser.add_argument('--skip_bids_validator', help='Whether or not to perform BIDS dataset validation',
-#                    action='store_true')
-parser.add_argument('-v', '--version', action='version',
-                    version='BIDS-App example version {}'.format(__version__))
-
-
-args = parser.parse_args()
+cmp_parser = parser.get()
+args = cmp_parser.parse_args()
 
 print('> BIDS dataset: {}'.format(args.bids_dir))
 
