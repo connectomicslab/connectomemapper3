8023be7951c2e050083779fc438c74e92112dac2
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2019-08-21 12:57:26 +0200

    MAINT: build main docker image in build_bidsapp.sh script

--- build_bidsapp.sh
+++ build_bidsapp.sh
@@ -2,15 +2,20 @@
 CMP_BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 echo $CMP_BUILD_DATE
 
-VERSION=$(python get_version.py)
+VERSION=v$(python get_version.py)
 echo $VERSION
 
 VCS_REF=$(git rev-parse --verify HEAD)
 echo $VCS_REF
 
-MAIN_DOCKER=sebastientourbier/connectomemapper-ubuntu16.04:$VERSION
-MAIN_DOCKER="sebastientourbier/connectomemapper-ubuntu16.04:3.0.0-beta-singularity"
+MAIN_DOCKER="sebastientourbier/connectomemapper-ubuntu16.04:${VERSION}"
 echo $MAIN_DOCKER
+
+docker build --rm --build-arg BUILD_DATE=$CMP_BUILD_DATE \
+				  --build-arg VCS_REF=$VCS_REF \
+				  --build-arg VERSION=$VERSION \
+				  -t "${MAIN_DOCKER}" ./ubuntu16.04 \
+
 
 docker build --no-cache --rm --build-arg BUILD_DATE=$CMP_BUILD_DATE \
                              --build-arg VERSION=$VERSION \
