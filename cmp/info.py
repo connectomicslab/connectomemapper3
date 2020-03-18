f92b4d7a19c4422de68c369d8e83d67ec6dc1206
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2020-03-18 11:06:15 +0100

    MAINT: updated version tag and expected release date

--- cmp/info.py
+++ cmp/info.py
@@ -3,7 +3,7 @@
 _version_major = 3
 _version_minor = 0
 _version_micro = 0
-_version_extra = '-beta-20200227'
+_version_extra = '-beta-20200320'
 __release_date__ = '14.05.2019'
 
 __minor_version__ = "%s.%s" % (_version_major,
