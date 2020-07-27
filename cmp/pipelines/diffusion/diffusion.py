9bb6cce4f741f3371f8181cfaa9a9033369a35e4
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2020-07-27 14:48:09 +0200

    FIX: correct model in maps generated by dipy tensor fit

--- cmp/pipelines/diffusion/diffusion.py
+++ cmp/pipelines/diffusion/diffusion.py
@@ -747,6 +747,9 @@ class DiffusionPipeline(Pipeline):
                 ('ADC', self.subject + '_model-DTI_MD'),
                 ('FA', self.subject + '_model-DTI_FA'),
                 ('diffusion_preproc_resampled_fa', self.subject + '_model-DTI_FA'),
+                ('diffusion_preproc_resampled_ad', self.subject + '_model-DTI_AD'),
+                ('diffusion_preproc_resampled_md', self.subject + '_model-DTI_MD'),
+                ('diffusion_preproc_resampled_rd', self.subject + '_model-DTI_RD'),
                 ('shore_gfa.nii.gz', '{}_model-SHORE_GFA.nii.gz'.format(self.subject)),
                 ('shore_msd.nii.gz', '{}_model-SHORE_MSD.nii.gz'.format(self.subject)),
                 ('shore_rtop_signal.nii.gz',
@@ -759,9 +762,6 @@ class DiffusionPipeline(Pipeline):
                 # Dipy
                 ('spherical_harmonics_image.nii.gz',
                  '{}_model-CSD_diffmodel.nii.gz'.format(self.subject)),
-                ('diffusion_preproc_resampled_ad', self.subject + '_model-CSD_AD'),
-                ('diffusion_preproc_resampled_md', self.subject + '_model-CSD_MD'),
-                ('diffusion_preproc_resampled_rd', self.subject + '_model-CSD_RD'),
                 # Mrtrix
                 ('shm_coeff.nii.gz', '{}_model-CSD_diffmodel.nii.gz'.format(self.subject)),
                 # MRtrix
