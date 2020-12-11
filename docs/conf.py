6318337fe8c02cdccc78cd4e30df4a6e57e7cf5f
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2020-12-11 16:36:43 +0100

    DOC: try an other way to access cmp on path for autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -32,8 +32,7 @@ source_suffix = ['.rst', '.md']
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../cmp'))
-sys.path.insert(1, os.path.abspath('../cmtklib'))
+sys.path.insert(0, ';'.join(os.path.abspath('../cmp'), os.path.abspath('../cmtklib')))
 
 # -- General configuration -----------------------------------------------------
 
