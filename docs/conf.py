cb0f1c0b97b70bd639eead002813ac7997e9b2a4
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2020-12-11 16:28:48 +0100

    DOC: fix path to module cmp

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ source_suffix = ['.rst', '.md']
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../cmp'))
-sys.path.insert(0, os.path.abspath('../cmtklib'))
+sys.path.insert(1, os.path.abspath('../cmtklib'))
 
 # -- General configuration -----------------------------------------------------
 
