5c0069c7859888e90dc17c28342f5931107a94fc
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2020-12-24 13:34:26 +0100

    VERSION: update release date in cmp/info.py [skip ci]

--- cmp/info.py
+++ cmp/info.py
@@ -4,7 +4,7 @@ _version_major = 3
 _version_minor = 0
 _version_micro = 0
 _version_extra = '-RC2'
-__release_date__ = '01.10.2020'
+__release_date__ = '24.12.2020'
 
 __minor_version__ = "%s.%s" % (_version_major,
                                _version_minor)
