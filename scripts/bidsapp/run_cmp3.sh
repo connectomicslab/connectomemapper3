7d0ed0b39de07b8d1795b53d48dcf81618697243
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2021-01-14 11:43:36 +0100

    MAINT: clean BIDSApp entrypoint script run_cmp3.sh [skip ci]

--- scripts/bidsapp/run_cmp3.sh
+++ scripts/bidsapp/run_cmp3.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo User: $(id -un $USER) && echo Group: $(id -gn $USER) &&
 . "$FSLDIR/etc/fslconf/fsl.sh" &&
-export && \
-echo "SHELL: $SHELL" && \
-echo "PATH: $PATH" && \
 . activate "${CONDA_ENV}" &&
 xvfb-run -s "-screen 0 900x900x24 -ac +extension GLX -noreset" -a python /app/connectomemapper3/run.py "$@"
