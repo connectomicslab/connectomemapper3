f5b394ed42ca6d1e98908edc075e08a02201e9c6
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2021-02-26 09:56:55 +0100

    WIP: Better handle loading of configuration parameter types

--- cmtklib/config.py
+++ cmtklib/config.py
@@ -415,6 +415,8 @@ def set_pipeline_attributes_from_config(pipeline, config, debug=True):
                                     conf_value = bool(conf_value)
                                 elif isinstance(getattr(sub_config, sub_key), list):
                                     conf_value = eval(conf_value)
+                                elif isinstance(getattr(sub_config, sub_key), dict):
+                                    conf_value = eval(conf_value)
                                 elif isinstance(getattr(sub_config, sub_key), int):
                                     conf_value = int(conf_value)
                                     if debug:
@@ -442,6 +444,8 @@ def set_pipeline_attributes_from_config(pipeline, config, debug=True):
                             elif isinstance(getattr(stage.config, key), bool):
                                 conf_value = bool(conf_value)
                             elif isinstance(getattr(stage.config, key), list):
+                                conf_value = eval(conf_value)
+                            elif isinstance(getattr(stage.config, key), dict):
                                 conf_value = eval(conf_value)
                             elif isinstance(getattr(stage.config, key), int):
                                 conf_value = int(conf_value)
