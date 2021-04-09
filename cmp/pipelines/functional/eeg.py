0d586817dfe1d2c239e00d302f08aa5f8e3d2827
Joan Rue <joan.rue.q@gmail.com>
2021-04-09 16:14:51 +0200

    FIX: wrong identation

--- cmp/pipelines/functional/eeg.py
+++ cmp/pipelines/functional/eeg.py
@@ -94,10 +94,10 @@ class EEGPipeline(Pipeline):
 		cmp_common.Pipeline.__init__(self, project_info)
 
 		self.stages['EEGPreparer'].config.cmp3_dir = os.path.join(self.derivatives_directory,'cmp')
-        self.stages['EEGPreparer'].config.cartool_dir = os.path.join(self.derivatives_directory,'cartool')
+		self.stages['EEGPreparer'].config.cartool_dir = os.path.join(self.derivatives_directory,'cartool')
 
-    	self.stages['EEGLoader'].config.eeg_format = self.stages['EEGPreparer'].config.eeg_format
-        self.stages['EEGLoader'].config.invsol_format = self.stages['EEGPreparer'].config.invsol_format
+		self.stages['EEGLoader'].config.eeg_format = self.stages['EEGPreparer'].config.eeg_format
+		self.stages['EEGLoader'].config.invsol_format = self.stages['EEGPreparer'].config.invsol_format
 
 		#self.stages['EEGPreparer'].config.on_trait_change(self.update_eeg_format, 'eeg_format')
 
