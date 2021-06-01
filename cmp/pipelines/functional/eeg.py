389f47cda39cbb76b97a004405b541d4400dc9bc
Sébastien Tourbier <sebastien.tourbier1@gmail.com>
2021-06-01 10:35:31 +0200

    STY: Review code style of `eeg.py` for example

--- cmp/pipelines/functional/eeg.py
+++ cmp/pipelines/functional/eeg.py
@@ -68,13 +68,17 @@ class EEGPipeline(Pipeline):
 
 		if len(project_info.subject_sessions) > 0:
 			self.global_conf.subject_session = project_info.subject_session
-			self.subject_directory = os.path.join(project_info.base_directory,
-												  project_info.subject,
-												  project_info.subject_session)
+			self.subject_directory = os.path.join(
+				project_info.base_directory,
+			        project_info.subject,
+			        project_info.subject_session
+			)
 		else:
 			self.global_conf.subject_session = ''
-			self.subject_directory = os.path.join(project_info.base_directory,
-												  project_info.subject)
+			self.subject_directory = os.path.join(
+				project_info.base_directory,
+				project_info.subject
+			)
 
 		self.derivatives_directory = os.path.abspath(project_info.output_directory)
 
@@ -83,13 +87,11 @@ class EEGPipeline(Pipeline):
 		else:
 			self.output_directory = os.path.join(self.base_directory, "derivatives")
 
-		self.stages = {'EEGPreparer': EEGPreparerStage(bids_dir=project_info.base_directory,
-														   output_dir=self.output_directory),
-					   'EEGLoader': EEGLoaderStage(bids_dir=project_info.base_directory,
-														   output_dir=self.output_directory),
-					   'EEGInverseSolution': EEGInverseSolutionStage(bids_dir=project_info.base_directory,
-														   output_dir=self.output_directory),
-					   }
+		self.stages = {
+			'EEGPreparer': EEGPreparerStage(bids_dir=project_info.base_directory, output_dir=self.output_directory),
+		        'EEGLoader': EEGLoaderStage(bids_dir=project_info.base_directory, output_dir=self.output_directory),
+		        'EEGInverseSolution': EEGInverseSolutionStage(bids_dir=project_info.base_directory, output_dir=self.output_directory),
+		}
 		
 		cmp_common.Pipeline.__init__(self, project_info)
 
@@ -100,35 +102,34 @@ class EEGPipeline(Pipeline):
 		self.stages['EEGLoader'].config.invsol_format = self.stages['EEGPreparer'].config.invsol_format
 
 		#self.stages['EEGPreparer'].config.on_trait_change(self.update_eeg_format, 'eeg_format')
-
 		#self.stages['EEGLoader'].config.on_trait_change(self.update_eeg_format, 'eeg_format')	
 		#self.stages['EEGInverseSolution'].config.on_trait_change(self.update_parcellation_scheme, 'parcellation_scheme')
 		
 	def check_config(self):
 		raise NotImplementedError
+
 	def check_input(self,layout, gui=True):
 		"""Check if input of the eeg pipeline are available.
 
-        Parameters
-        ----------
-        layout : bids.BIDSLayout
-            Instance of BIDSLayout
+		Parameters
+		----------
+		layout : bids.BIDSLayout
+		    Instance of BIDSLayout
 
-        gui : traits.Bool
-            Boolean used to display different messages
-            but not really meaningful anymore since the GUI
-            components have been migrated to `cmp.bidsappmanager`
+		gui : traits.Bool
+		    Boolean used to display different messages
+		    but not really meaningful anymore since the GUI
+		    components have been migrated to `cmp.bidsappmanager`
 
-        Returns
-        -------
-
-        valid_inputs : traits.Bool
-            True if inputs are available
-        """
-		print('**** Check Inputs is not still implemented ****')
+		Returns
+		-------
+		valid_inputs : traits.Bool
+		    True if inputs are available
+		"""
+		print('**** Check Inputs is still not implemented ****')
 		eeg_available = False
 		epochs_available = False
-		#valid_inputs = False
+		# valid_inputs = False
 		valid_inputs = True
 		return valid_inputs
 
@@ -136,13 +137,18 @@ class EEGPipeline(Pipeline):
 		raise NotImplementedError
 
 	def create_pipeline_flow(self, cmp_deriv_subject_directory, nipype_deriv_subject_directory):    		
-		datasource = pe.Node(interface=util.IdentityInterface(
-					fields=['base_directory',
-							'subject',
-							'cmp_deriv_subject_directory',
-							'nipype_deriv_subject_directory',							
-							], 
-					mandatory_inputs=True), name="datasource")
+		datasource = pe.Node(
+			interface=util.IdentityInterface(
+				fields=[
+					'base_directory',
+					'subject',
+					'cmp_deriv_subject_directory',
+					'nipype_deriv_subject_directory',							
+				], 
+				mandatory_inputs=True
+			),
+			name="datasource"
+		)
 
 		datasource.inputs.base_directory = self.base_directory
 		datasource.inputs.subject = self.subject
@@ -150,13 +156,51 @@ class EEGPipeline(Pipeline):
 		datasource.inputs.nipype_deriv_subject_directory = nipype_deriv_subject_directory
 
 		if self.stages['EEGPreparer'].config.eeg_format == '.set':
-			datasource.inputs.epochs = [os.path.join(self.base_directory,'derivatives','eeglab',self.subject,'eeg',self.subject+'_task-FACES_desc-preproc_eeg.set')]
-			datasource.inputs.behav_file = [os.path.join(self.base_directory,'derivatives','eeglab',self.subject,'eeg',self.subject+'_task-FACES_events.txt')]
-			datasource.inputs.epochs_fif_fname = os.path.join(self.base_directory,'derivatives','cmp',self.subject,'eeg',self.subject+'_epo.fif')
+			datasource.inputs.epochs = [
+				os.path.join(
+					self.base_directory,
+					'derivatives',
+					'eeglab',
+					self.subject,
+					'eeg',
+					self.subject+'_task-FACES_desc-preproc_eeg.set'
+				)
+			]
+			datasource.inputs.behav_file = [
+				os.path.join(
+					self.base_directory,
+					'derivatives',
+					'eeglab',
+					self.subject,'eeg',self.subject+'_task-FACES_events.txt'
+				)
+			]
+			datasource.inputs.epochs_fif_fname = os.path.join(
+				self.base_directory,
+				'derivatives',
+				'cmp',
+				self.subject,'eeg',self.subject+'_epo.fif'
+			)
 	
-		datasource.inputs.roi_ts_file = os.path.join(self.base_directory,'derivatives','cmp',self.subject,'eeg',self.subject+'_rtc_epo.npy')
+		datasource.inputs.roi_ts_file = os.path.join(
+			self.base_directory,
+			'derivatives',
+			'cmp',
+			self.subject,
+			'eeg',
+			self.subject+'_rtc_epo.npy'
+		)
+	
 		if self.stages['EEGPreparer'].config.parcellation == 'Lausanne2008':
-			datasource.inputs.parcellation = [os.path.join(self.base_directory,'derivatives','cmp',self.subject,'anat',self.subject+'_label-L2008_desc-scale2_atlas.nii.gz')]
+			datasource.inputs.parcellation = [
+				os.path.join(
+					self.base_directory,
+					'derivatives',
+					'cmp',
+					self.subject,
+					'anat',
+					self.subject+'_label-L2008_desc-scale2_atlas.nii.gz'
+				)
+			]
 
 		datasource.inputs.output_query = dict()
 
@@ -168,70 +212,38 @@ class EEGPipeline(Pipeline):
 		self.clear_stages_outputs()
 
 		# Create common_flow
-		eeg_flow = pe.Workflow(name='eeg_pipeline', 
-							   base_dir= os.path.abspath(nipype_deriv_subject_directory))
-
-		
+		eeg_flow = pe.Workflow(
+			name='eeg_pipeline', 
+			base_dir= os.path.abspath(nipype_deriv_subject_directory)
+		)
+	
 		preparer_flow = self.create_stage_flow("EEGPreparer")        
 		loader_flow = self.create_stage_flow("EEGLoader")    
 		invsol_flow = self.create_stage_flow("EEGInverseSolution")    
 	
-
-		eeg_flow.connect([
-						  (datasource, preparer_flow, 
-						   [
-							('epochs','inputnode.epochs'),
-							('subject','inputnode.subject'),
-							('behav_file','inputnode.behav_file'),
-							('parcellation','inputnode.parcellation'),
-							('epochs_fif_fname','inputnode.epochs_fif_fname'),
-							('output_query','inputnode.output_query'),
-							]),
-						 ])
-
-		eeg_flow.connect([
-						  (datasource, loader_flow, 
-						   [('base_directory','inputnode.base_directory'),
-						    ('subject','inputnode.subject'),
-							]),
-						 ])
-
-		eeg_flow.connect([
-						  (preparer_flow, loader_flow, 
-						   [('outputnode.output_query','inputnode.output_query'),
-						    ('outputnode.derivative_list','inputnode.derivative_list'),
-							]),
-						 ])
-
-		
-		eeg_flow.connect([
-						  (loader_flow, invsol_flow, 
-						   [
-							('outputnode.EEG','inputnode.eeg_ts_file'),                            
-							('outputnode.rois','inputnode.rois_file'),
-							('outputnode.src','inputnode.src_file'),
-							('outputnode.invsol','inputnode.invsol_file'),
-							]),
-						 ])
-
-		eeg_flow.connect([
-						  (datasource, invsol_flow, 
-						   [
-							('roi_ts_file','inputnode.roi_ts_file'),
-							]),
-						 ])
-
-		eeg_flow.connect([
-						  (preparer_flow, invsol_flow, 
-						   [
-							('outputnode.invsol_params','inputnode.invsol_params'),
-							]),
-						 ])
-
-
-		eeg_flow.connect([			
-			(invsol_flow, sinker, [("outputnode.roi_ts_file", "eeg.@roi_ts_file")]),
-		])
+		# fmt: off
+		eeg_flow.connect(
+			[
+				(datasource, preparer_flow, [('epochs','inputnode.epochs'),
+							     ('subject','inputnode.subject'),
+							     ('behav_file','inputnode.behav_file'),
+							     ('parcellation','inputnode.parcellation'),
+							     ('epochs_fif_fname','inputnode.epochs_fif_fname'),
+							     ('output_query','inputnode.output_query')]),
+				(datasource, loader_flow, [('base_directory','inputnode.base_directory'),
+				    			   ('subject','inputnode.subject')]),
+				(preparer_flow, loader_flow, [('outputnode.output_query','inputnode.output_query'),
+						    	      ('outputnode.derivative_list','inputnode.derivative_list')]),
+				(loader_flow, invsol_flow, [('outputnode.EEG','inputnode.eeg_ts_file'),                            
+							    ('outputnode.rois','inputnode.rois_file'),
+						            ('outputnode.src','inputnode.src_file'),
+							    ('outputnode.invsol','inputnode.invsol_file')]),
+				(datasource, invsol_flow, [('roi_ts_file','inputnode.roi_ts_file')]),
+				(preparer_flow, invsol_flow, [('outputnode.invsol_params','inputnode.invsol_params')]),
+				(invsol_flow, sinker, [("outputnode.roi_ts_file", "eeg.@roi_ts_file")]),
+			]
+		)
+		# fmt: on
 
 		self.flow = eeg_flow
 		return eeg_flow
@@ -249,56 +261,76 @@ class EEGPipeline(Pipeline):
 
 		if self.global_conf.subject_session == '':
 			cmp_deriv_subject_directory = os.path.join(
-				self.output_directory, "cmp", self.subject)
+				self.output_directory, "cmp", self.subject
+			)
 			nipype_deriv_subject_directory = os.path.join(
-				self.output_directory, "nipype", self.subject)
+				self.output_directory, "nipype", self.subject
+			)
 		else:
-			cmp_deriv_subject_directory = os.path.join(self.output_directory, "cmp", self.subject,
-													   self.global_conf.subject_session)
-			nipype_deriv_subject_directory = os.path.join(self.output_directory, "nipype", self.subject,
-														  self.global_conf.subject_session)
+			cmp_deriv_subject_directory = os.path.join(
+				self.output_directory, "cmp", self.subject, self.global_conf.subject_session
+			)
+			nipype_deriv_subject_directory = os.path.join(
+				self.output_directory, "nipype", self.subject, self.global_conf.subject_session
+			)
 
 			self.subject = "_".join(
-				(self.subject, self.global_conf.subject_session))
+				(self.subject, self.global_conf.subject_session)
+			)
 
 		if not os.path.exists(os.path.join(nipype_deriv_subject_directory, "eeg_pipeline")):
 			try:
-				os.makedirs(os.path.join(
-					nipype_deriv_subject_directory, "eeg_pipeline"))
+				os.makedirs(
+					os.path.join(nipype_deriv_subject_directory, "eeg_pipeline")
+				)
 			except os.error:
-				print("%s was already existing" % os.path.join(
-					nipype_deriv_subject_directory, "eeg_pipeline"))
+				print(
+					"%s was already existing" % os.path.join(
+						nipype_deriv_subject_directory,
+						"eeg_pipeline"
+					)
+				)
 
 		# Initialization
 		if os.path.isfile(os.path.join(nipype_deriv_subject_directory, "eeg_pipeline", "pypeline.log")):
-			os.unlink(os.path.join(nipype_deriv_subject_directory,
-								   "eeg_pipeline", "pypeline.log"))
+			os.unlink(
+				os.path.join(nipype_deriv_subject_directory, "eeg_pipeline", "pypeline.log"))
 		config.update_config(
-			{'logging': {'log_directory': os.path.join(nipype_deriv_subject_directory, "eeg_pipeline"),
-						 'log_to_file': True},
-			 'execution': {'remove_unnecessary_outputs': False,
-						   'stop_on_first_crash': True,
-						   'stop_on_first_rerun': False,
-						   'use_relative_paths': True,
-						   'crashfile_format': "txt"}
-			 })
+			{
+				'logging': {
+					'log_directory': os.path.join(nipype_deriv_subject_directory, "eeg_pipeline"),
+					'log_to_file': True
+				},
+			 	'execution': {
+					'remove_unnecessary_outputs': False,
+					 'stop_on_first_crash': True,
+					 'stop_on_first_rerun': False,
+					 'use_relative_paths': True,
+					 'crashfile_format': "txt"
+				}
+			}
+		)
 		logging.update_logging(config)
 
 		iflogger = logging.getLogger('nipype.interface')
 		iflogger.info("**** Processing ****")
 
-		eeg_flow = self.create_pipeline_flow(cmp_deriv_subject_directory=cmp_deriv_subject_directory,
-											  nipype_deriv_subject_directory=nipype_deriv_subject_directory)
-		eeg_flow.write_graph(graph2use='colored',
-							  format='svg', simple_form=True)
+		eeg_flow = self.create_pipeline_flow(
+			cmp_deriv_subject_directory=cmp_deriv_subject_directory,
+			nipype_deriv_subject_directory=nipype_deriv_subject_directory
+		)
+		eeg_flow.write_graph(graph2use='colored', format='svg', simple_form=True)
 
 		if self.number_of_cores != 1:
-			eeg_flow.run(plugin='MultiProc',
-						  plugin_args={'n_procs': self.number_of_cores})
+			eeg_flow.run(
+				plugin='MultiProc',
+			        plugin_args={
+					'n_procs': self.number_of_cores
+				}
+			)
 		else:
 			eeg_flow.run()
 
 		iflogger.info("**** Processing finished ****")
 
 		return True
-
