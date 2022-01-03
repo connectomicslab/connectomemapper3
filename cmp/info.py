c3d929805db5c83fde38b4ee1cbcea6aa9fe9df4
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2022-01-03 15:59:39 +0100

    VERSION: Update version to 3.0.1

--- cmp/info.py
+++ cmp/info.py
@@ -7,9 +7,9 @@
 
 _version_major = 3
 _version_minor = 0
-_version_micro = 0
+_version_micro = 1
 _version_extra = ""
-__release_date__ = "24.12.2021"
+__release_date__ = "05.01.2022"
 
 __minor_version__ = "%s.%s" % (_version_major, _version_minor)
 
