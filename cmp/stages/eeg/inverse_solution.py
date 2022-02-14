efa054c8dd1a33b59e56b73e561b94a48575e36b
Sébastien Tourbier <sebastien.tourbier1@gmail.com>
2022-02-14 13:21:16 +0100

    STY: Reformat with BLACK and review the flow.connect block

--- cmp/stages/eeg/inverse_solution.py
+++ cmp/stages/eeg/inverse_solution.py
@@ -8,7 +8,7 @@
 
 # General imports
 import os
-from traits.api import *
+from traits.api import HasTraits, Enum
 
 # Nipype imports
 import nipype.pipeline.engine as pe
@@ -20,8 +20,6 @@ from cmtklib.interfaces.mne import CreateCov, CreateFwd, MNEInverseSolution
 
 
 class EEGInverseSolutionConfig(HasTraits):
-    invsol_format = Enum('Cartool-LAURA', 'Cartool-LORETA', 'mne-sLORETA',
-                         desc='Cartool vs mne')
     """Class used to store configuration parameters of a :class:`~cmp.stages.eeg.inverse_solution.EEGInverseSolutionStage` instance.
 
     Attributes
@@ -53,15 +51,32 @@ class EEGInverseSolutionStage(Stage):
     cmp.pipelines.functional.eeg.EEGPipeline
     cmp.stages.eeg.inverse_solution.EEGInverseSolutionConfig
     """
+
     def __init__(self, bids_dir, output_dir):
         """Constructor of a :class:`~cmp.stages.parcellation.parcellation.ParcellationStage` instance."""
-        self.name = 'eeg_inverse_solution_stage'
+        self.name = "eeg_inverse_solution_stage"
         self.bids_dir = bids_dir
         self.output_dir = output_dir
         self.config = EEGInverseSolutionConfig()
-        self.inputs = ["bids_dir","subject","eeg_ts_file", "epochs_fif_fname", "rois_file", "src_file", "invsol_file",
-                       "lamda", "svd_params", "roi_ts_file", "invsol_params", "bem_file", "noise_cov_fname",
-                       "trans_fname", "fwd_fname","inv_fname","parcellation"]
+        self.inputs = [
+            "bids_dir",
+            "subject",
+            "eeg_ts_file",
+            "epochs_fif_fname",
+            "rois_file",
+            "src_file",
+            "invsol_file",
+            "lamda",
+            "svd_params",
+            "roi_ts_file",
+            "invsol_params",
+            "bem_file",
+            "noise_cov_fname",
+            "trans_fname",
+            "fwd_fname",
+            "inv_fname",
+            "parcellation",
+        ]
         self.outputs = ["roi_ts_file", "fwd_fname"]
 
     def create_workflow(self, flow, inputnode, outputnode):
@@ -78,74 +93,63 @@ class EEGInverseSolutionStage(Stage):
         outputnode : nipype.interfaces.utility.IdentityInterface
             Identity interface describing the outputs of the stage
         """
+        if self.config.invsol_format.split("-")[0] == "Cartool":
             invsol_node = pe.Node(CartoolInverseSolutionROIExtraction(), name="invsol")
-            flow.connect([(inputnode, invsol_node,
-                           [('eeg_ts_file', 'eeg_ts_file'),
-                            ('rois_file', 'rois_file'),
-                            ('invsol_file', 'invsol_file'),
-                            ('invsol_params', 'invsol_params'),
-                            ('roi_ts_file', 'roi_ts_file')
-                            ]
-                           )])
-            flow.connect([(invsol_node, outputnode,
-                 [
-                  ('roi_ts_file','roi_ts_file'),
-                 ]
-                    )])
-            
-        elif self.config.invsol_format.split('-')[0] == "mne":
-            
-            covmat_node = pe.Node(CreateCov(), name='createcov') # compute the noise covariance
-            fwd_node = pe.Node(CreateFwd(), name='createfwd') # compute the forward solution 
-            invsol_node = pe.Node(MNEInverseSolution(), name="invsol") # compute the inverse operator 
-            
-            flow.connect([(inputnode, covmat_node,
-                   [('noise_cov_fname','noise_cov_fname'),
-                    ('epochs_fif_fname','epochs_fif_fname')
-                  ]
-                    )])  
-            
-            flow.connect([(inputnode, fwd_node,
-                   [('fwd_fname','fwd_fname'),
-                    ('src_file','src'),
-                    ('bem_file','bem'),
-                    ('trans_fname','trans_fname'),
-                    ('epochs_fif_fname','epochs_fif_fname')
-                  ]
-                    )]) 
-            
-            flow.connect([(inputnode, invsol_node,
-                   [('subject', 'subject'),
-                    ('bids_dir', 'bids_dir'),
-                    ('fwd_fname','fwd_fname'),
-                    ('inv_fname','inv_fname'),
-                    ('src_file','src_file'),
-                    ('bem_file','bem_file'),
-                    ('epochs_fif_fname','epochs_fif_fname'),
-                    ('parcellation','parcellation'),
-                    ('roi_ts_file', 'roi_ts_file')
-                   ]
-                    )])      
-            
-            # use dummy variables "has_run" to enforce order of nodes although they don't produce outputs 
+            # fmt: off
+            flow.connect(
+                [
+                    (inputnode, invsol_node, [("eeg_ts_file", "eeg_ts_file"),
+                                              ("rois_file", "rois_file"),
+                                              ("invsol_file", "invsol_file"),
+                                              ("invsol_params", "invsol_params"),
+                                              ("roi_ts_file", "roi_ts_file")]),
+                    (invsol_node, outputnode, [("roi_ts_file", "roi_ts_file")])
+                ]
+            )
+            # fmt: on
+
+        elif self.config.invsol_format.split("-")[0] == "mne":
+
+            covmat_node = pe.Node(CreateCov(), name="createcov")  # compute the noise covariance
+            fwd_node = pe.Node(CreateFwd(), name="createfwd")  # compute the forward solution
+            invsol_node = pe.Node(MNEInverseSolution(), name="invsol")  # compute the inverse operator
+
+            # fmt: off
+            flow.connect(
+                [
+                    (inputnode, covmat_node, [("noise_cov_fname", "noise_cov_fname"),
+                                              ("epochs_fif_fname", "epochs_fif_fname")]),
+                    (inputnode, fwd_node, [("fwd_fname", "fwd_fname"),
+                                           ("src_file", "src"),
+                                           ("bem_file", "bem"),
+                                           ("trans_fname", "trans_fname"),
+                                           ("epochs_fif_fname", "epochs_fif_fname")]),
+                    (inputnode, invsol_node, [("subject", "subject"),
+                                              ("bids_dir", "bids_dir"),
+                                              ("fwd_fname", "fwd_fname"),
+                                              ("inv_fname", "inv_fname"),
+                                              ("src_file", "src_file"),
+                                              ("bem_file", "bem_file"),
+                                              ("epochs_fif_fname", "epochs_fif_fname"),
+                                              ("parcellation", "parcellation"),
+                                              ("roi_ts_file", "roi_ts_file")])
+                ]
+            )
+            # fmt: on
+
+            # use dummy variables "has_run" to enforce order of nodes although they don't produce outputs
             # (outputs are files with fixed names that are defined in eeg.py)
-            flow.connect([(covmat_node,invsol_node,
-                           [('has_run','cov_has_run'),
-                            ('noise_cov_fname','noise_cov_fname')
-                            ]
-                           )])
-            
-            flow.connect([(fwd_node,invsol_node,
-                           [('has_run','fwd_has_run')
-                            ]
-                           )])
-
-            flow.connect([(invsol_node, outputnode,
-                  [('fwd_fname','fwd_fname'),
-                   ('roi_ts_file','roi_ts_file'),
-                  ]
-                    )])
-
+            # fmt: off
+            flow.connect(
+                [
+                    (covmat_node, invsol_node, [("has_run", "cov_has_run"),
+                                                ("noise_cov_fname", "noise_cov_fname")]),
+                    (fwd_node, invsol_node, [("has_run", "fwd_has_run")]),
+                    (invsol_node, outputnode, [("fwd_fname", "fwd_fname"),
+                                               ("roi_ts_file", "roi_ts_file")])
+                ]
+            )
+            # fmt: on
 
     def define_inspect_outputs(self):
         raise NotImplementedError
@@ -158,5 +162,5 @@ class EEGInverseSolutionStage(Stage):
         `True` if the stage has been run successfully
         """
         if self.config.eeg_format == ".set":
-            if self.config.invsol_format.split('-')[0] == "Cartool":
+            if self.config.invsol_format.split("-")[0] == "Cartool":
                 return os.path.exists(self.config.roi_ts_file)
