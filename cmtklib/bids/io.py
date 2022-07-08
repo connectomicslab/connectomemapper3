f63db143b52a54b4d4a06f46ed763fd1dabb54c1
Sébastien Tourbier <sebastien.tourbier1@gmail.com>
2022-07-08 18:02:40 +0200

    ENH: Add new classes to cmtklib.bids.io to describe the EEG files

--- cmtklib/bids/io.py
+++ cmtklib/bids/io.py
@@ -8,7 +8,7 @@
 import csv
 import os
 import json
-from traits.api import (HasTraits, Str)
+from traits.api import (HasTraits, Str, Enum)
 
 from cmp.info import __version__
 from nipype import __version__ as nipype_version
@@ -31,11 +31,20 @@ class CustomBIDSFile(HasTraits):
     toolbox_derivatives_dir : Str
         Toolbox folder name in the `derivatives/` of the BIDS dataset
 
+    datatype: Enum(["anat", "dwi", "func", "eeg"])
+        BIDS data type
+
     suffix : Str
         Filename suffix e.g. `sub-01_T1w.nii.gz` has suffix `T1w`
 
     acquisition : Str
         Label used in `_acq-<label>_`
+
+    task : Str
+        Label used in `_task-<label>_`
+
+    rec : Str
+        Label used in `_rec-<label>_`
 
     res : Str
         Label used in `_res-<label>_`
@@ -53,44 +62,55 @@ class CustomBIDSFile(HasTraits):
         Label used in `_desc-<label>_`
 
     """
-    toolbox_derivatives_dir = Str
-    suffix = Str
-    acquisition = Str
-    res = Str
-    extension = Str
-    atlas = Str
-    label = Str
-    desc = Str
+    toolbox_derivatives_dir = Str(desc="Toolbox folder name in the derivatives/ directory of the BIDS dataset")
+    datatype = Enum(["anat", "dwi", "func", "eeg"], desc="")
+    suffix = Str(desc="")
+    acquisition = Str(desc="")
+    rec = Str(desc="")
+    extension = Str(desc="")
+    atlas = Str(desc="")
+    res = Str(desc="")
+    label = Str(desc="")
+    desc = Str(desc="Label used in _desc-<label>_")
+    task = Str(desc="")
 
     def __init__(
             self,
             p_toolbox_derivatives_dir="",
+            p_datatype="",
             p_suffix="",
             p_extension="",
             p_acquisition="",
+            p_rec="",
             p_atlas="",
             p_res="",
             p_label="",
-            p_desc=""
+            p_desc="",
+            p_task=""
     ):
         self.toolbox_derivatives_dir = p_toolbox_derivatives_dir
+        self.datatype = p_datatype
         self.suffix = p_suffix
         self.extension = p_extension
         self.acquisition = p_acquisition
+        self.rec = p_rec
         self.atlas = p_atlas
         self.res = p_res
         self.label = p_label
         self.desc = p_desc
+        self.task = p_task
 
     def __str__(self):
         msg = "{"
         msg += f' "custom_derivatives_dir": "{self.toolbox_derivatives_dir}"'
-        if self.suffix:
-            msg += f', "suffix": "{self.suffix}"'
-        if self.extension:
-            msg += f', "extension": "{self.extension}"'
+        if self.datatype:
+            msg += f', "datatype": "{self.datatype}"'
+        if self.task:
+            msg += f', "task": "{self.task}"'
         if self.acquisition:
             msg += f', "acquisition": "{self.acquisition}"'
+        if self.rec:
+            msg += f', "reconstruction": "{self.rec}"'
         if self.atlas:
             msg += f', "atlas": "{self.atlas}"'
         if self.res:
@@ -99,6 +119,10 @@ class CustomBIDSFile(HasTraits):
             msg += f', "label": "{self.label}"'
         if self.desc:
             msg += f', "desc": "{self.desc}"'
+        if self.suffix:
+            msg += f', "suffix": "{self.suffix}"'
+        if self.extension:
+            msg += f', "extension": "{self.extension}"'
         msg += "}"
         return msg
 
@@ -116,7 +140,7 @@ class CustomBIDSFile(HasTraits):
         return self.toolbox_derivatives_dir
 
     def get_filename_path(self, base_dir, subject, session=None, debug=True):
-        """Return the number of regions by reading its associated TSV side car file describing the nodes.
+        """Return the filename path without extension of the represented BIDS file.
 
         Parameters
         ----------
@@ -131,31 +155,35 @@ class CustomBIDSFile(HasTraits):
             (Default: None)
 
         debug: bool
-            Debug mode (Extra outputed messages) if `True`
+            Debug mode (Extra output messages) if `True`
 
         """
-        # Build path to BIDS TSV side car of the parcellation file
+        # Build path to custom parcellation file, dropping the extension
         filepath = os.path.join(
             base_dir,
             subject
         )
-
         fname = f'{subject}'
         if session is not None and session != "":
+            # Handle the subject/session filename and structure
             fname += f'_{session}'
             filepath = os.path.join(filepath, session)
+        if self.task is not None and self.task != "":
+            fname += f'_task-{self.task}'
         if self.label is not None and self.label != "":
             fname += f'_label-{self.label}'
         if self.atlas is not None and self.atlas != "":
             fname += f'_atlas-{self.atlas}'
         if self.res is not None and self.res != "":
             fname += f'_res-{self.res}'
+        if self.rec is not None and self.rec != "":
+            fname += f'_rec-{self.rec}'
         if self.desc is not None and self.desc != "":
             fname += f'_desc-{self.desc}'
         fname += f'_{self.suffix}'
-        filepath = os.path.join(filepath, "anat", fname)
+        filepath = os.path.join(filepath, self.datatype, fname)
         if debug:  # pragma: no cover
-            print(f" .. DEBUG : Generated parcellation file path (no extension) = {filepath}")
+            print(f" .. DEBUG : Generated file path (no extension) = {filepath}")
         return filepath
 
 
@@ -163,7 +191,7 @@ class CustomParcellationBIDSFile(CustomBIDSFile):
     """Represent a custom parcellation files in the form `sub-<label>_atlas-<label>[_res-<label>]_dseg.nii.gz`."""
 
     def __init__(self):
-        super().__init__(p_suffix="dseg", p_atlas="L2018", p_extension="nii.gz")
+        super().__init__(p_datatype="anat", p_suffix="dseg", p_atlas="L2018", p_extension="nii.gz")
 
     def get_nb_of_regions(self, bids_dir, subject, session=None, debug=True):
         """Return the number of regions by reading its associated TSV side car file describing the nodes.
@@ -181,7 +209,7 @@ class CustomParcellationBIDSFile(CustomBIDSFile):
             (Default: None)
 
         debug: bool
-            Debug mode (Extra outputed messages) if `True`
+            Debug mode (Extra output messages) if `True`
         """
         # Build path to BIDS TSV side car of the parcellation file
         parc_filepath = self.get_filename_path(
@@ -208,32 +236,116 @@ class CustomBrainMaskBIDSFile(CustomBIDSFile):
     """Represent a custom brain mask in the form `sub-<label>_desc-brain_mask.nii.gz`."""
 
     def __init__(self):
-        super().__init__(p_suffix="mask", p_desc="brain", p_extension="nii.gz")
+        super().__init__(p_datatype="anat", p_suffix="mask", p_desc="brain", p_extension="nii.gz")
 
 
 class CustomWMMaskBIDSFile(CustomBIDSFile):
     """Represent a custom white-matter mask in the form `sub-<label>_label-WM_dseg.nii.gz`."""
 
     def __init__(self):
-        super().__init__(p_suffix="dseg", p_label="WM", p_extension="nii.gz")
+        super().__init__(p_datatype="anat", p_suffix="dseg", p_label="WM", p_extension="nii.gz")
 
 
 class CustomGMMaskBIDSFile(CustomBIDSFile):
     """Represent a custom gray-matter mask in the form `sub-<label>_label-GM_dseg.nii.gz`."""
 
     def __init__(self):
-        super().__init__(p_suffix="dseg", p_label="GM", p_extension="nii.gz")
+        super().__init__(p_datatype="anat", p_suffix="dseg", p_label="GM", p_extension="nii.gz")
 
 
 class CustomCSFMaskBIDSFile(CustomBIDSFile):
     """Represent a custom CSF mask in the form `sub-<label>_label-CSF_dseg.nii.gz`."""
 
     def __init__(self):
-        super().__init__(p_suffix="dseg", p_label="CSF", p_extension="nii.gz")
+        super().__init__(p_datatype="anat", p_suffix="dseg", p_label="CSF", p_extension="nii.gz")
 
 
 class CustomAparcAsegBIDSFile(CustomBIDSFile):
     """Represent a custom BIDS-formatted Freesurfer aparc+aseg file in the form `sub-<label>_desc-aparcaseg_dseg.nii.gz`."""
 
     def __init__(self):
-        super().__init__(p_suffix="dseg", p_desc="aparcaseg", p_extension="nii.gz")
+        super().__init__(p_datatype="anat", p_suffix="dseg", p_desc="aparcaseg", p_extension="nii.gz")
+
+
+class CustomEEGPreprocBIDSFile(CustomBIDSFile):
+    """Represent a custom BIDS-formatted preprocessed EEG file in the form `sub-<label>_task-<label>_desc-preproc_eeg.[set|fif]`."""
+    extension = Enum(['set', 'fif'])
+
+    def __init__(self):
+        super().__init__(p_datatype="eeg", p_suffix="eeg", p_desc="preproc", p_extension=self.extension)
+
+
+class CustomEEGEpochsBIDSFile(CustomBIDSFile):
+    """Represent a custom BIDS-formatted EEG Epochs file in .set or .fif format."""
+    extension = Enum(['set', 'fif'])
+
+    def __init__(self):
+        super().__init__(p_datatype="eeg", p_suffix="epo", p_desc="preproc", p_extension=self.extension)
+
+
+class CustomEEGEventsBIDSFile(CustomBIDSFile):
+    """Represent a custom BIDS-formatted EEG task events file in the form `sub-<label>_task-<label>_events.tsv`."""
+
+    def __init__(self):
+        super().__init__(p_datatype="eeg", p_suffix="events", p_extension="tsv")
+
+    def extract_event_ids_from_json_sidecar(self, base_dir, subject, session=None, debug=True):
+        events_json_file = self.get_filename_path(base_dir, subject, session, debug) + ".json"
+        with open(events_json_file, "r") as f:
+            json_content = json.load(f)
+            # Reformat the information about tasks and integer encoding
+            # to the format MNE expects
+            trial_name = json_content["trial_type"]["Levels"].keys()
+            trial_id = json_content["trial_type_id"]["Levels"].keys()
+            event_ids = {f"{t_name}": f"{t_id}" for t_name, t_id in zip(trial_name, trial_id)}
+            if debug:
+                print(f"  .. DEBUG: Event_ids for Epochs extraction: {event_ids}")
+        return event_ids
+
+
+class CustomEEGElectrodesBIDSFile(CustomBIDSFile):
+    """Represent a custom BIDS-formatted EEG electrodes file in the form `sub-<label>_task-<label>_electrodes.tsv`."""
+
+    def __init__(self):
+        super().__init__(p_datatype="eeg", p_suffix="electrodes", p_extension="tsv")
+
+
+class CustomEEGCartoolElectrodesBIDSFile(CustomBIDSFile):
+    """Represent a custom BIDS-formatted electrode file produced by Cartool, in the form `sub-<label>_eeg.xyz`."""
+
+    def __init__(self):
+        super().__init__(p_datatype="eeg", p_suffix="eeg", p_extension="xyz")
+
+
+class CustomEEGCartoolSpiBIDSFile(CustomBIDSFile):
+    """Represent a custom BIDS-formatted Source Point Irregularly spaced file produced by Cartool, in the form `sub-<label>_eeg.spi`."""
+
+    def __init__(self):
+        super().__init__(p_datatype="eeg", p_suffix="eeg", p_extension="spi")
+
+
+class CustomEEGCartoolMapSpiRoisBIDSFile(CustomBIDSFile):
+    """Represent a custom BIDS-formatted spi / rois mapping file in the form `sub-<label>_eeg.pickle.rois`."""
+
+    def __init__(self):
+        super().__init__(p_datatype="eeg", p_suffix="eeg", p_extension="pickle.rois")
+
+
+class CustomEEGMNETransformBIDSFile(CustomBIDSFile):
+    """Represent a custom BIDS-formatted electrode transform file in the form `sub-<label>_trans.fif`."""
+
+    def __init__(self):
+        super().__init__(p_datatype="eeg", p_suffix="trans", p_extension="fif")
+
+
+class CustomEEGCartoolInvSolBIDSFile(CustomBIDSFile):
+    """Represent a custom BIDS-formatted inverse solution file produced by Cartool in the form `sub-<label>_eeg.[LAURA|LORETA].is`."""
+    esi_method = Enum(['LAURA', 'LORETA'], desc="EEG Source Imaging method")
+
+    def __init__(self):
+        super().__init__(p_datatype="eeg", p_suffix="eeg", p_extension=f"{self.esi_method}.is")
+
+    def _esi_method_changed(self, new):
+        """Update extension when esi method is modified."""
+        self.extension = f"{new}.is"
+
